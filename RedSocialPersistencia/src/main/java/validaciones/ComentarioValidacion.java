5f4ca2d3d6925a0b8cdd26ee0287fc5b4c9206b5
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2023-06-13 23:13:29 -0700

    Ajustes Documentación v1.0.1
    Cambios importantes!

--- RedSocialPersistencia/src/main/java/validaciones/ComentarioValidacion.java
+++ RedSocialPersistencia/src/main/java/validaciones/ComentarioValidacion.java
@@ -17,26 +17,30 @@ public class ComentarioValidacion {
 
     public ComentarioValidacion() {
     }
-
+    /**
+     * Método validarComentario que recibe como parámetro un comentario
+     * @param comentario
+     * @return validarComentario
+     */
     public List<String> validarComentario(Comentario comentario) {
         List<String> errores = new ArrayList<>();
-
+        //Condición if comentario igual a nulo return String
         if (comentario == null) {
             errores.add("El comentario esta vacio");
         }
-
+        //Condición if fecha de comentario igual a nulo return String
         if (comentario.getFechaHora() == null) {
             errores.add("El campo FechaHora es requerido");
         }
-
+        //Condición if contenido comentario igual a nulo o vacío return String
         if (comentario.getContenido() == null || comentario.getContenido().isEmpty()) {
             errores.add("El campo Contenido es requerido");
         }
-
+        //Condición if comentario de usuario normal igual a nulo return String
         if (comentario.getUsuarioNormal() == null) {
             errores.add("El campo UsuarioNormal es requerido");
         }
-
+        //Condición if comentario de publicación igual a nulo return String
         if (comentario.getPublicacionComun() == null) {
             errores.add("El campo PublicacionComun es requerido");
         }
--- RedSocialPersistencia/src/main/java/validaciones/PublicacionValidacion.java
+++ RedSocialPersistencia/src/main/java/validaciones/PublicacionValidacion.java
@@ -20,38 +20,46 @@ public class PublicacionValidacion {
     public PublicacionValidacion() {
     }
 
-    //Validaciones 
+    /**
+     * Método List validarPublicacion que recibe como parámetro una publicación
+     * @param publicacion
+     * @return Lista de Caracteres llamada errores.
+     */
     public List<String> validarPublicacion(Publicacion publicacion) {
         List<String> errores = new ArrayList<>();
 
-        // Validar los atributos de la publicación común
+        //Condición if fechaHora publicación igual a nulo return String
         if (publicacion.getFechaHoraCreacion() == null) {
             errores.add("El campo FechaHoraCreacion es requerido");
         }
-
+        //Condición if Título publicación igual a nulo return String
         if (publicacion.getTitulo() == null || publicacion.getTitulo().isEmpty()) {
             errores.add("El campo Titulo es requerido");
         }
-
+        //Condición if contenido publicación igual a nulo return String
         if (publicacion.getContenido() == null || publicacion.getContenido().isEmpty()) {
             errores.add("El campo Contenido es requerido");
         }
 
         return errores;
     }
-
+    /**
+     * Método List validarPublicacionComún que recibe como parámetro una publicación común
+     * @param publicacionComun
+     * @return  Lista de Caracteres llamada errores.
+     */
     public List<String> validarPublicacionComun(Comun publicacionComun) {
         List<String> errores = new ArrayList<>();
 
-        // Validar los atributos de la publicación común
+        //Condición if fechaHora publicación igual a nulo return String
         if (publicacionComun.getFechaHoraCreacion() == null) {
             errores.add("El campo FechaHoraCreacion es requerido");
         }
-
+        //Condición if Título publicación igual a nulo o vacío return String
         if (publicacionComun.getTitulo() == null || publicacionComun.getTitulo().isEmpty()) {
             errores.add("El campo Titulo es requerido");
         }
-
+        //Condición if contenido publicación igual a nulo o vacío return String
         if (publicacionComun.getContenido() == null || publicacionComun.getContenido().isEmpty()) {
             errores.add("El campo Contenido es requerido");
         }
